e0203dd10daef3829836878dc123a5aa4099711a
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 09:28:47 +0500

    Initial commit

---
+++ Percentage Calculation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percentage_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your roll no:");
+            int roll_no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the marks of COMPUTER FUNDAMENTALS out of 100:");
+            double cf = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the marks of COMPUTER PROGRAMMING out of 100:");
+            double cp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the marks of APPLIED PHYSICS out of 100:");
+            double ap = Convert.ToDouble(Console.ReadLine());
+            double total_marks = Convert.ToDouble(cf+cp+ap);
+            double percentage = Convert.ToDouble((total_marks/300)*100);
+            Console.WriteLine("Dear " + name + "! You have scored " + percentage + "% " + "with " + total_marks + " marks.");
+        }
+    }
+}
